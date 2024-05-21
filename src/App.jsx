61ecafe65280764ff510270848fb931d891a059b
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-21 19:07:51 +0300

    navigation 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home/Home";
+import Movies from "./pages/Movies/Movies";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+      </Routes>
     </>
   );
 }
