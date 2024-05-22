f7b90411feeeebbb237c0f74da1609bfcff165df
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-22 16:55:08 +0300

    add poster to details page

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { getMovieDetails } from "../../movies-api";
 
 const MovieDetailsPage = () => {
-  const [movieData, setMovieData] = useState({});
+  const [movieData, setMovieData] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
@@ -14,25 +14,34 @@ const MovieDetailsPage = () => {
     getMovieById();
   }, [id]);
 
-  const { title, release_date, popularity, overview, genres } = movieData;
+  const { title, release_date, popularity, overview, genres, poster_path } =
+    movieData || {};
 
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <h2>{`${title} (${release_date})`}</h2>
-        <p>{`User score - ${parseInt(popularity)}%`}</p>
-        <h3>Overview</h3>
-        <p>{overview}</p>
-        <h3>Genres</h3>
-        <ul>
-          {Array.isArray(genres) &&
-            genres.map(({ name, id }) => <li key={id}>{name}</li>)}
-        </ul>
-      </div>
-    </div>
+    <>
+      {movieData && (
+        <div>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+              alt="Movie Poster"
+            />
+          </div>
+          <div>
+            <h2>{`${title} (${release_date})`}</h2>
+            <p>{`User score - ${parseInt(popularity)}%`}</p>
+            <h3>Overview</h3>
+            <p>{overview}</p>
+            <h3>Genres</h3>
+            <ul>
+              {genres.map(({ name, id }) => (
+                <li key={id}>{name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
