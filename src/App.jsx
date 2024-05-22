ef581ed5d496f917ad92dc4b45ae0fcc9933d9ec
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-22 15:21:37 +0300

    product descr

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,4 @@ function App() {
 export default App;
 
 // Where to put header
+// try - catch?
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieDetails } from "../../movies-api";
 
 const MovieDetailsPage = () => {
+  const [movieData, setMovieData] = useState({});
   const { id } = useParams();
 
-  const getMovieById = async () => {
-    const movieData = await getMovieDetails(id);
-    console.log(movieData);
-  };
-  getMovieById();
+  useEffect(() => {
+    const getMovieById = async () => {
+      const movieData = await getMovieDetails(id);
+      setMovieData(movieData);
+    };
 
-  return <div>Details</div>;
+    getMovieById();
+  }, [id]);
+
+  const { title, release_date, popularity, overview, genres } = movieData;
+
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h2>{`${title} (${release_date})`}</h2>
+        <p>{`User score - ${parseInt(popularity)}`}</p>
+        <h3>Overview</h3>
+        <p>{overview}</p>
+        <h3>Genres</h3>
+        <ul>
+          {Array.isArray(genres) &&
+            genres.map(({ name, id }) => <li key={id}>{name}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetailsPage;
